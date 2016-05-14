e62a53156e1573c745ad2d84b3079ed7a3b55933
Johnfordman <ndoulou.jean@gmail.com>
2016-05-14 11:54:47 +0200

    Init projet

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+	browserSync = require('browser-sync').create();
+	
+
+gulp.task('css', function() {
+        return sass('assets/sass/*.scss',{sourcemap: true}, { style: 'expanded' })
+        .pipe(autoprefixer('last 2 version','ie9'))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream({match: 'dist/*.css'}))
+        .pipe(notify({ message: 'css task complete' }));    
+});
+
+gulp.task('cssmin', function() {
+    return sass('assets/sass/main.scss',{ style: 'expanded' })
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(notify({ message: 'css min task complete' })); 
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('assets/scripts/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+gulp.task('watch', function() {
+	
+	 browserSync.init({
+         proxy: "localhost:80/projet_mobilefirst"
+        //server: {
+        //    baseDir: "./"
+        //}
+    });
+  
+  gulp.watch('assets/sass/**/*.scss', ['css']);
+  gulp.watch("dist/css/*.css").on('change', browserSync.reload);
+  gulp.watch('assets/scripts/**/*.js', ['scripts']);
+
+});    
---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Mibile first</title>
+	<link rel="stylesheet" type="text/css" href="dist/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="dist/css/main.css">
+	<link href='https://fonts.googleapis.com/css?family=Signika:400,300' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+    <div class="wrap-container">
+        <div class="header-player">
+            <div class="section-player">
+        		<div class="section-player-bg">
+        			<div class="overlay"></div>
+        		</div>
+        		<div class="section-player-info">
+        			<div class="player-picture img-circle"><img src="assets/images/torres-profile.jpg" width="110" height="110" alt="fernando torres"></div>
+        			<div class="player-name"><span>Fernando Torres</span></div>
+        			<div class="container-team">
+        			<?php echo file_get_contents("assets/images/icons/pin.svg"); ?>
+        				<span class="player-team">Athlecitco Madrid,Spain</span>
+        			</div>
+        		</div>
+        	</div>
+
+        	<section class="section-palmares">
+        		<div class="selection-item">
+        			<span class="icon"><?php echo file_get_contents("assets/images/icons/icn-cupworld.svg"); ?></span>
+        			<span class="chiffre">9</span>
+        		</div>
+        		<div class="selection-item">
+        			<span class="icon"><?php echo file_get_contents("assets/images/icons/icn-cupworld.svg"); ?></span>
+
+        			<span class="chiffre">92</span>
+        		</div>
+        		<div class="selection-item">
+        			<span class="icon"><?php echo file_get_contents("assets/images/icons/icn-cupworld.svg"); ?></span>
+        			<span class="chiffre">203</span>
+        		</div>
+        	</section>
+        </div>
+
+    	<section class="section-news">
+    		<div class="container-news">
+    			<?php $i= 0; ?>
+    			<?php while($i <= 4): ?>
+    			<div class="single-news">
+    				<span class="news-circle"></span>
+    				<div class="news-date">
+    					<span class="news-date-day">Vendredi</span>
+    					<span class="news-date-hours">11:30</span>
+    				</div>
+    				<div class="news-text">
+    					<p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete</p>
+    				</div>
+    			</div>
+    			<?php $i++; ?>
+    			<?php endwhile; ?>
+    		</div>
+    	</section>
+    </div>
+
+</body>
+</html>
